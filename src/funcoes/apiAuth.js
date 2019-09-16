c05def31002e9a59bb8cd27426388349e39d9111
Mhagner <m.hagner.sp@gmail.com>
2019-09-16 17:20:32 -0300

    final

--- src/funcoes/apiAuth.js
+++ src/funcoes/apiAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getToken } from './services';
 
 const apiAuth = axios.create({
-    baseURL: REACT_APP_API_AUTH //'http://localhost:3003/auth'
+    baseURL: process.env.REACT_APP_API_AUTH //'http://localhost:3003/auth'
 })
 
 export default apiAuth
